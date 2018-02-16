356cce2fd5bd9a6c3a40340913253b78577202e3
ArunKumarPal <arunpal692@gmail.com>
2018-02-16 22:02:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/action1',function(req,res){
+  res.send("seciond");  
+});
 app.get('/action2',function(req,res){
+  res.send("seciond");  
+});
+app.get('/action3',function(req,res){
   res.send("seciond");  
 });
 
