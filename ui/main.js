664310f89dc1297e4da271452acce0f3fa83ee32
ArunKumarPal <arunpal692@gmail.com>
2018-02-18 11:46:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var element=document.getElementById('main-text');
+elemtnt.innerHTML='New value';
+//moving img
+var img=document.getElementById('madi');
+img.onClick = function(){
+    img.style.marginLeft='100px';
+};
